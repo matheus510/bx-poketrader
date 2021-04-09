c1f0263b2aade32029de2449344e7904611c2991
Matheus <matheus510.fonseca@gmail.com>
2021-04-09 01:25:44 -0300

    fix on db path

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,4 +1,4 @@
-import dbConnect from '../../utils/dbConnect'
+import dbConnect from '../../utils/dbConnection'
 import Trade from '../../models/Trade'
 
 export default async function handler(req, res) {
